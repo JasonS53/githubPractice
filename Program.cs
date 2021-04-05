bad0359bd10f17b81aaa00a3ac39dfd81f66dc6e
Jason <j_schubb53@yahoo.com>
2021-04-05 10:36:07 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace bootcampday1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            int ageF = int.Parse(age);
+            Console.WriteLine("What is your favourite colour?");
+            string colour = Console.ReadLine();
+
+            Console.WriteLine(name + " your age is "+ ageF + " and your favourite colour is "  + colour + ".");
+
+        }
+    }
+}
